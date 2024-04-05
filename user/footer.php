17c6450457534b02782fe54c0d5f975f79c6e3d2
Chinmayk12 <karodpatichinmay12@gmail.com>
2024-04-05 14:32:28 +0530

    USer Footer Updated

--- user/footer.php
+++ user/footer.php
@@ -1,56 +1,293 @@
- <!-- START: Footer-->
- <footer class="site-footer">
- <p>Design and Develop by : 1.Chinmay Karodpati</p>
+
+<style type="text/css">
+    body {
+    background: #fbfbfd;
+            }
+
+            .new_footer_area {
+                background: #fbfbfd;
+            }
+
+
+            .new_footer_top {
+                padding: 120px 0px 270px;
+                position: relative;
+                  overflow-x: hidden;
+            }
+            .new_footer_area .footer_bottom {
+                padding-top: 6px;
+                padding-bottom: 50px;
+            }
+            .footer_bottom {
+                font-size: 14px;
+                font-weight: 300;
+                line-height: 20px;
+                color: #7f88a6;
+                padding: 27px 0px;
+            }
+            .new_footer_top .company_widget p {
+                font-size: 16px;
+                font-weight: 300;
+                line-height: 28px;
+                color: #6a7695;
+                margin-bottom: 20px;
+            }
+            .new_footer_top .company_widget .f_subscribe_two .btn_get {
+                border-width: 1px;
+                margin-top: 20px;
+            }
+            .btn_get_two:hover {
+                background: transparent;
+                color: #5e2ced;
+            }
+            .btn_get:hover {
+                color: #fff;
+                background: #6754e2;
+                border-color: #6754e2;
+                -webkit-box-shadow: none;
+                box-shadow: none;
+            }
+            a:hover, a:focus, .btn:hover, .btn:focus, button:hover, button:focus {
+                text-decoration: none;
+                outline: none;
+            }
+
+
+
+            .new_footer_top .f_widget.about-widget .f_list li a:hover {
+                color: #5e2ced;
+            }
+            .new_footer_top .f_widget.about-widget .f_list li {
+                margin-bottom: 11px;
+            }
+            .f_widget.about-widget .f_list li:last-child {
+                margin-bottom: 0px;
+            }
+            .f_widget.about-widget .f_list li {
+                margin-bottom: 15px;
+            }
+            .f_widget.about-widget .f_list {
+                margin-bottom: 0px;
+            }
+            .new_footer_top .f_social_icon a {
+                width: 44px;
+                height: 44px;
+                line-height: 43px;
+                background: transparent;
+                border: 1px solid #e2e2eb;
+                font-size: 24px;
+            }
+            .f_social_icon a {
+                width: 46px;
+                height: 46px;
+                border-radius: 50%;
+                font-size: 14px;
+                line-height: 45px;
+                color: #858da8;
+                display: inline-block;
+                background: #ebeef5;
+                text-align: center;
+                -webkit-transition: all 0.2s linear;
+                -o-transition: all 0.2s linear;
+                transition: all 0.2s linear;
+            }
+            .ti-facebook:before {
+                content: "\e741";
+            }
+            .ti-twitter-alt:before {
+                content: "\e74b";
+            }
+            .ti-vimeo-alt:before {
+                content: "\e74a";
+            }
+            .ti-pinterest:before {
+                content: "\e731";
+            }
+
+            .btn_get_two {
+                -webkit-box-shadow: none;
+                box-shadow: none;
+                background: #5e2ced;
+                border-color: #5e2ced;
+                color: #fff;
+            }
+
+            .btn_get_two:hover {
+                background: transparent;
+                color: #5e2ced;
+            }
+
+            .new_footer_top .f_social_icon a:hover {
+                background: #5e2ced;
+                border-color: #5e2ced;
+              color:white;
+            }
+            .new_footer_top .f_social_icon a + a {
+                margin-left: 4px;
+            }
+            .new_footer_top .f-title {
+                margin-bottom: 30px;
+                color: #263b5e;
+            }
+            .f_600 {
+                font-weight: 600;
+            }
+            .f_size_18 {
+                font-size: 18px;
+            }
+            h1, h2, h3, h4, h5, h6 {
+                color: #4b505e;
+            }
+            .new_footer_top .f_widget.about-widget .f_list li a {
+                color: #6a7695;
+            }
+
+
+            .new_footer_top .footer_bg {
+                position: absolute;
+                bottom: 0;
+                background: url("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEigB8iI5tb8WSVBuVUGc9UjjB8O0708X7Fdic_4O1LT4CmLHoiwhanLXiRhe82yw0R7LgACQ2IhZaTY0hhmGi0gYp_Ynb49CVzfmXtYHUVKgXXpWvJ_oYT8cB4vzsnJLe3iCwuzj-w6PeYq_JaHmy_CoGoa6nw0FBo-2xLdOPvsLTh_fmYH2xhkaZ-OGQ/s16000/footer_bg.png") no-repeat scroll center 0;
+                width: 100%;
+                height: 266px;
+            }
+
+            .new_footer_top .footer_bg .footer_bg_one {
+                background: url("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEia0PYPxwT5ifToyP3SNZeQWfJEWrUENYA5IXM6sN5vLwAKvaJS1pQVu8mOFFUa_ET4JuHNTFAxKURFerJYHDUWXLXl1vDofYXuij45JZelYOjEFoCOn7E6Vxu0fwV7ACPzArcno1rYuVxGB7JY6G7__e4_KZW4lTYIaHSLVaVLzklZBLZnQw047oq5-Q/s16000/volks.gif") no-repeat center center;
+                width: 330px;
+                height: 105px;
+              background-size:100%;
+                position: absolute;
+                bottom: 0;
+                left: 30%;
+                -webkit-animation: myfirst 22s linear infinite;
+                animation: myfirst 22s linear infinite;
+            }
+
+            .new_footer_top .footer_bg .footer_bg_two {
+                background: url("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhyLGwEUVwPK6Vi8xXMymsc-ZXVwLWyXhogZxbcXQYSY55REw_0D4VTQnsVzCrL7nsyjd0P7RVOI5NKJbQ75koZIalD8mqbMquP20fL3DxsWngKkOLOzoOf9sMuxlbyfkIBTsDw5WFUj-YJiI50yzgVjF8cZPHhEjkOP_PRTQXDHEq8AyWpBiJdN9SfQA/s16000/cyclist.gif") no-repeat center center;
+                width: 88px;
+                height: 100px;
+              background-size:100%;
+                bottom: 0;
+                left: 38%;
+                position: absolute;
+                -webkit-animation: myfirst 30s linear infinite;
+                animation: myfirst 30s linear infinite;
+            }
+
+
+
+            @-moz-keyframes myfirst {
+              0% {
+                left: -25%;
+              }
+              100% {
+                left: 100%;
+              }
+            }
+
+            @-webkit-keyframes myfirst {
+              0% {
+                left: -25%;
+              }
+              100% {
+                left: 100%;
+              }
+            }
+
+            @keyframes myfirst {
+              0% {
+                left: -25%;
+              }
+              100% {
+                left: 100%;
+              }
+            }
+</style>
+<footer class="new_footer_area bg_color">
+            <div class="new_footer_top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6">
+                            <div class="f_widget company_widget wow fadeInLeft" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInLeft;">
+                                <h3 class="f-title f_600 t_color f_size_18">Get in Touch</h3>
+                                <p>Contact us for any inquiries or assistance. We're here!</p>
+                                <form action="#" class="f_subscribe_two mailchimp" method="post" novalidate="true" _lpchecked="1">
+                                    <input type="text" name="EMAIL" class="form-control memail" placeholder="Email">
+                                    <button class="btn btn_get btn_get_two" type="submit">Subscribe</button>
+                                    <p class="mchimp-errmessage" style="display: none;"></p>
+                                    <p class="mchimp-sucmessage" style="display: none;"></p>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInLeft;">
+                                <h3 class="f-title f_600 t_color f_size_18">Explore</h3>
+                                <ul class="list-unstyled f_list">
+                                    <li><a href="index.php">Home</a></li>
+                                    <li><a href="products-category.php?type=creative">Creatives</a></li>
+                                    <li><a href="#">Scraps</a></li>
+                                    <li><a href="#">Community</a></li>
+                                    <li><a href="#">Shop</a></li>
+                                    <li><a href="#">Customized Products</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.6s" style="visibility: visible; animation-delay: 0.6s; animation-name: fadeInLeft;">
+                                <h3 class="f-title f_600 t_color f_size_18">Help</h3>
+                                <ul class="list-unstyled f_list">
+                                    <li><a href="#">FAQ</a></li>
+                                    <li><a href="#">Term &amp; conditions</a></li>
+                                    <li><a href="#">Reporting</a></li>
+                                    <li><a href="#">Documentation</a></li>
+                                    <li><a href="#">Support Policy</a></li>
+                                    <li><a href="#">Privacy</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="f_widget social-widget pl_70 wow fadeInLeft" data-wow-delay="0.8s" style="visibility: visible; animation-delay: 0.8s; animation-name: fadeInLeft;">
+                                <h3 class="f-title f_600 t_color f_size_18">Team SCRAP SWAP</h3>
+                                <div class="f_social_icon">
+                                    <a href="#" class="fab fa-facebook"></a>
+                                    <a href="#" class="fab fa-twitter"></a>
+                                    <a href="#" class="fab fa-linkedin"></a>
+                                    <a href="#" class="fab fa-pinterest"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer_bg">
+                    <div class="footer_bg_one"></div>
+                    <div class="footer_bg_two"></div>
+                </div>
+            </div>
+            <div class="footer_bottom">
+                <div class="container">
+                   
+                </div>
+            </div>
         </footer>
-        <!-- END: Footer-->
 
 
-        <!-- START: Back to top-->
-        <a href="#" class="scrollup text-center"> 
-            <i class="icon-arrow-up"></i>
-        </a>
-        <!-- END: Back to top-->
+
+        <a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
 
 
-        <!-- START: Template JS-->
-        <script src="dist/vendors/jquery/jquery-3.3.1.min.js"></script>
-        <script src="dist/vendors/jquery-ui/jquery-ui.min.js"></script>
-        <script src="dist/vendors/moment/moment.js"></script>
-        <script src="dist/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>    
-        <script src="dist/vendors/slimscroll/jquery.slimscroll.min.js"></script>
-        <!-- END: Template JS-->
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
 
-        <!-- START: APP JS-->
-        <script src="dist/js/app.js"></script>
-        <!-- END: APP JS-->
+    <!-- Contact Javascript File -->
+    <script src="mail/jqBootstrapValidation.min.js"></script>
+    <script src="mail/contact.js"></script>
 
-        <!-- START: Page Vendor JS-->
-        <script src="dist/vendors/raphael/raphael.min.js"></script>
-        <script src="dist/vendors/morris/morris.min.js"></script>
-        <script src="dist/vendors/chartjs/Chart.min.js"></script>
-        <script src="dist/vendors/starrr/starrr.js"></script>
-        <script src="dist/vendors/jquery-flot/jquery.canvaswrapper.js"></script>
-        <script src="dist/vendors/jquery-flot/jquery.colorhelpers.js"></script>
-        <script src="dist/vendors/jquery-flot/jquery.flot.js"></script>
-        <script src="dist/vendors/jquery-flot/jquery.flot.saturated.js"></script>
-        <script src="dist/vendors/jquery-flot/jquery.flot.browser.js"></script>
-        <script src="dist/vendors/jquery-flot/jquery.flot.drawSeries.js"></script>
-        <script src="dist/vendors/jquery-flot/jquery.flot.uiConstants.js"></script>
-        <script src="dist/vendors/jquery-flot/jquery.flot.legend.js"></script>
-        <script src="dist/vendors/jquery-flot/jquery.flot.pie.js"></script>        
-        <script src="dist/vendors/chartjs/Chart.min.js"></script>  
-        <script src="dist/vendors/jquery-jvectormap/jquery-jvectormap-2.0.3.min.js"></script>
-        <script src="dist/vendors/jquery-jvectormap/jquery-jvectormap-world-mill.js"></script>
-        <script src="dist/vendors/jquery-jvectormap/jquery-jvectormap-de-merc.js"></script>
-        <script src="dist/vendors/jquery-jvectormap/jquery-jvectormap-us-aea.js"></script>
-        <script src="dist/vendors/apexcharts/apexcharts.min.js"></script>
-        <!-- END: Page Vendor JS-->
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
 
-        <!-- START: Page JS-->
-        <script src="dist/js/home.script.js"></script>
-        <!-- END: Page JS-->
-    </body>
-    <!-- END: Body-->
-
-<!-- Mirrored from html.designstream.co.in/pick/html/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 08 Aug 2021 08:44:22 GMT -->
 </html>
